5be16c835674669717c4d64a413d0b6495f034cb
Maksim Palilov <palilovfox@gmail.com>
2024-10-21 23:34:39 +0300

    update

--- p99.sh
+++ p99.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-
-if ! command -v g++ &> /dev/null
-then
-    echo "Компилятор g++ не найден. Установите его для продолжения."
-    exit 1
-else
-    echo "Компилятор g++ найден."
-fi
-
 g++ -o hello main.cpp
-
-if [ $? -eq 0 ]; then
-    echo "Сборка прошла успешно."
-else
-    echo "Ошибка сборки."
-    exit 1
-fi
-
 echo "Запуск программы:"
 ./hello
 
